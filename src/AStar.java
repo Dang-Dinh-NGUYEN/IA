6847bd986296fc9c01ccfa8921e451ebb01670e1
Dang Dinh NGUYEN <nguyendangdinh12d211@gmail.com>
2023-04-11 19:55:50 +0200

    A*

--- src/AStar.java
+++ src/AStar.java
@@ -1,42 +1,79 @@
 import java.util.*;
 
-public class AStar extends Algorithm {
-    public AStar(Graph graph, Grid initialState, Grid finalState) {
-        super(graph, initialState, finalState);
-    }
-
-    @Override
-    void Handler() {
-        Set<String> visited = new LinkedHashSet<String>();
-        PriorityQueue<Grid> queue = new PriorityQueue<>((grid1, grid2) -> {
-            int value1 = Heuristic3.getValue(grid1, finalState);
-            int value2 = Heuristic3.getValue(grid2, finalState);
-            return Integer.compare(value1, value2);
-        });
-        queue.add(super.initialState);
-        visited.add(super.initialState.toString());
-        while (!queue.isEmpty()) {
-            Grid currentState = queue.poll();
-            if (currentState.isEqual(super.finalState)) {
-                super.finalState = currentState;
-                super.hasSolution = true;
-                System.out.println("explored " + visited.size() + " node.s");
-                return;
-            }
-            Square emptySquare = currentState.findEmptySquare();
-            for (CardinalDirection cardinalDirection : CardinalDirection.values()) {
-                if (emptySquare.hasNeighbor(cardinalDirection)) {
-                    Grid nextState = currentState.clone();
-                    Square empty = nextState.findEmptySquare();
-                    nextState.move(empty, cardinalDirection);
-                    super.graph.addVertex(nextState);
-                    super.graph.addEdge(currentState, nextState);
-                    if (!visited.contains(nextState.toString())) {
-                        visited.add(nextState.toString());
-                        queue.add(nextState);
-                    }
-                }
+public class AStar extends Algorithm{
+    PriorityQueue<Grid> priorityQueue = new PriorityQueue<>(new Comparator<Grid>() {
+        @Override
+        public int compare(Grid grid1, Grid grid2) {
+            int h1 = Heuristic2.getValue(initialState, grid1) + Heuristic3.getValue(grid1, finalState);
+            int h2 = Heuristic2.getValue(initialState, grid2) + Heuristic3.getValue(grid2, finalState);
+            if(h1 == h2){
+                return Integer.compare(Heuristic2.getValue(initialState, grid1), Heuristic2.getValue(initialState, grid2));
+            }else {
+                return Integer.compare(h1,h2);
             }
         }
+    });
+
+    public AStar (Graph graph, Grid initialState, Grid finalState){
+        super(graph,initialState,finalState);
     }
+
+    /*Relevez les bloques de commentaires dans la méthode suivante pour observer les étapes d'exécution sur la console*/
+    public void Handler() {
+        priorityQueue.offer(super.initialState);
+
+        while (!priorityQueue.isEmpty()){
+            Grid currentState = priorityQueue.poll();
+
+            if(!visited.contains(currentState.toString())) {
+
+                /*
+                System.out.println("CurentState: ");
+                currentState.PrintGrid();
+                System.out.printf("f = g + h = %d + %d = %d\n",
+                        Heuristic2.getValue(initialState,currentState),Heuristic3.getValue(currentState,finalState),
+                        (Heuristic2.getValue(initialState,currentState) + Heuristic3.getValue(currentState,finalState)));
+                System.out.println("Exploring states: ");
+                 */
+
+                if (currentState.isEqual(super.finalState)) {
+                    super.finalState = currentState;
+                    super.hasSolution = true;
+                    break;
+                }
+                Square emptySquare = currentState.findEmptySquare();
+
+                for (CardinalDirection cardinalDirection : CardinalDirection.values()) {
+                    if (emptySquare.hasNeighbor(cardinalDirection)) {
+                        Grid nextState = currentState.clone();
+                        Square empty = nextState.findEmptySquare();
+                        nextState.move(empty, cardinalDirection);
+
+                        if (!visited.contains(nextState.toString())) {
+                            super.graph.addVertex(nextState);
+                            super.graph.addEdge(currentState, nextState);
+                            priorityQueue.offer(nextState);
+
+                            /*
+                            nextState.PrintGrid();
+                            System.out.printf("f = g + h = %d + %d = %d\n",
+                                    Heuristic2.getValue(initialState,nextState),Heuristic3.getValue(nextState,finalState),
+                                    (Heuristic2.getValue(initialState,nextState) + Heuristic3.getValue(nextState,finalState)));
+                            System.out.println();
+                             */
+                        }
+                    }
+                }
+
+                /*
+                System.out.println("-------------------------------------------------------");
+                */
+
+                visited.add(currentState.toString());
+            }
+        }
+        System.out.println("explored: " + visited.size() + " node.s");
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static final String ANSI_CYAN = "\u001B[36m";
 
     public static void main(String[] args) throws Exception {
-        Game taquin = new Game("C:\\Users\\Dang Dinh NGUYEN\\Documents\\L3_INFO\\S6\\IA\\grilles\\grilles\\taquin_3x3.grid");
+        Game taquin = new Game("C:\\Users\\Dang Dinh NGUYEN\\Documents\\L3_INFO\\S6\\IA\\grilles\\grilles\\taquin_5x5f.grid");
 
         /*PARCOURS EN LARGEUR (SANS HEURISTIQUE)*/
-
+        /*
         BFS parcoursLargeur = new BFS((new Graph(taquin.getInitialGrid())), taquin.getInitialGrid(), taquin.getFinalGrid());
         taquin.solver(parcoursLargeur);
         System.out.println();
-
+        */
 
         /*PARCOURS EN PROFONDEUR (SANS HEURISTIQUE)*/
         /*
@@ -24,23 +24,30 @@ public class Main {
         */
 
         /*PARCOURS EN LARGEUR (EN UTILISANT HEURISTIQUE)*/
-
+        /*
         BFSH parcoursLargeurH = new BFSH((new Graph(taquin.getInitialGrid())), taquin.getInitialGrid(), taquin.getFinalGrid());
         taquin.solver(parcoursLargeurH);
         System.out.println();
-
+        */
 
         /*PARCOURS EN PROFONDEUR (EN UTILISANT HEURISTIQUE)*/
-
+        /*
         DFSH parcoursProfondH = new DFSH((new Graph(taquin.getInitialGrid())), taquin.getInitialGrid(), taquin.getFinalGrid());
         taquin.solver(parcoursProfondH);
         System.out.println();
-
+        */
 
         /*LA RECHERCHE LE MEILLEUR D'ABORD*/
 
         Greedy greedy = new Greedy((new Graph(taquin.getInitialGrid())), taquin.getInitialGrid(), taquin.getFinalGrid());
         taquin.solver(greedy);
+        System.out.println();
+
+
+        /*L'ALGORITHME A ETOILE*/
+
+        AStar aStar = new AStar((new Graph(taquin.getInitialGrid())), taquin.getInitialGrid(), taquin.getFinalGrid());
+        taquin.solver(aStar);
         System.out.println();
 
     }
